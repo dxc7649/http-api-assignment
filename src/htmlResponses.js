21211fb230aa0a4a8ec9c69cc66324424d79854e
David Chen <dxc7649@g.rit.edu>
2020-02-10 01:49:14 -0500

    second change to assignment unfinished

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,31 +3,13 @@ const fs = require('fs'); // pull in the file system module
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
-};
-
-const getStatus = (request, response, acceptedTypes) => {
-  const statusInfo = {
-    status: 'mr.cat',
-    response: 4,
-  };
-
-  if (acceptedTypes[0] === 'text/xml') {
-    let responseXML = '<response>';
-    responseXML = `${responseXML} <status>${statusInfo.status}</status>`;
-    responseXML = `${responseXML} <response>${statusInfo.response}</response>`;
-    responseXML = `${responseXML} </response>`;
-
-    return respond(request, response, responseXML, 'text/xml');
-  }
-
-  const statusString = JSON.stringify(stausInfo);
-  return respond(request, response, statusString, 'application/json');
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.write(index);
+    response.end();
 };
 
 module.exports = {
-  getIndex,
-  getStatus,
+    getIndex,
 };
